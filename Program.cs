cad9b42d9c278b393f231c0fc4ed74bb0520fd14
Francisco Cezar <102926017+franciscocezar@users.noreply.github.com>
2022-04-18 21:40:25 -0300

    Testando o uso de uma Classe

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProjetoRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo x = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            x.Largura = double.Parse(Console.ReadLine());
+            x.Altura = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("AREA = " + x.Area().ToString("F2"));
+            Console.WriteLine("PERÍMETRO = " + x.Perimetro().ToString("F2"));
+            Console.WriteLine("DIAGONAL = " + x.Diagonal().ToString("F2"));
+
+
+
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProjetoRetangulo
+{
+    public class Retangulo
+    {
+
+        public double Largura, Altura;
+
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+
+        public double Diagonal()
+        {
+            return Math.Sqrt((Math.Pow(Altura, 2) + Math.Pow(Largura, 2)));
+        }
+    }
+}
